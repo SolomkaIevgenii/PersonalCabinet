05d43ac3f58071fff3787eb9823c45625577702f
Solomka Ievgenii <solomka1989@gmail.com>
2018-05-04 21:51:30 +0300

    Test100500-2

--- app/src/main/java/com/example/john/pc_2/LoginActivity.java
+++ app/src/main/java/com/example/john/pc_2/LoginActivity.java
@@ -1,8 +1,11 @@
+///////jbnjkbhkbnlk l
+
+
 package com.example.john.pc_2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
