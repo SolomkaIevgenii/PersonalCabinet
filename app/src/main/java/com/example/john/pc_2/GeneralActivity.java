1c0c3f4b1df277ee61c1a63a71a464093a4cc176
Solomka Ievgenii <solomka1989@gmail.com>
2018-05-07 00:38:01 +0300

    06.05 Немного поправил

--- app/src/main/java/com/example/john/pc_2/GeneralActivity.java
+++ app/src/main/java/com/example/john/pc_2/GeneralActivity.java
@@ -23,6 +23,7 @@ public class GeneralActivity extends AppCompatActivity {
         setContentView(R.layout.activity_general);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(getBottomNavigationListener());
     }
 
@@ -44,29 +45,4 @@ public class GeneralActivity extends AppCompatActivity {
             }
         };
     }
-
 }
-        /*
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    case R.id.app_KioskMain:
-                        Toast.makeText(GeneralActivity.this, "Тут должен появится мониторинг терминалов", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(GeneralActivity.this, "Но увы...", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.app_NewsMain:
-                        Toast.makeText(GeneralActivity.this, "Тут должены быть новости", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(GeneralActivity.this, "Но увы...", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.app_NotificationsMain:
-                        Toast.makeText(GeneralActivity.this, "Тут когдато будут заявки и сообщения", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(GeneralActivity.this, "Но увы...", Toast.LENGTH_SHORT).show();
-                        break;
-            }
-            return false;
-            }
-        });
-        */
