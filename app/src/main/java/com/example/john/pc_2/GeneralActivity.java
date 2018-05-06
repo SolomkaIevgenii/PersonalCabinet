889bf3a1fd32b130f76b6b2c61452efe3d516390
Solomka Ievgenii <solomka1989@gmail.com>
2018-05-07 02:06:06 +0300

    07.05 оздал несколько фрагментов. Разобратся как отображать

--- app/src/main/java/com/example/john/pc_2/GeneralActivity.java
+++ app/src/main/java/com/example/john/pc_2/GeneralActivity.java
@@ -2,20 +2,31 @@ package com.example.john.pc_2;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 //import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.john.pc_2.fragmant.KioskFragment;
+import com.example.john.pc_2.fragmant.NewsFragment;
+
 //import java.util.List;
 
-public class GeneralActivity extends AppCompatActivity {
+public class GeneralActivity extends FragmentActivity {
 
     /*Логин и пароль с LoginActivity*/
     public static final String EXTRA_KEY_LOGIN = "Логин";
     public static final String EXTRA_KEY_PASSWORD = "Пароль";
+
+    private KioskFragment kioskFragment;
+    private NewsFragment newsFragment;
+
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,9 @@ public class GeneralActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(getBottomNavigationListener());
+
+        kioskFragment = new KioskFragment();
+        newsFragment = new NewsFragment();
     }
 
     @NonNull
@@ -33,8 +47,14 @@ public class GeneralActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                transaction = manager.beginTransaction();
+
                 if (item.getItemId() == R.id.app_KioskMain){
                     findViewById(R.id.recycleView).setVisibility(View.VISIBLE);
+                }
+                else if (item.getItemId()==R.id.app_NewsMain){
+
+                    //transaction.add(R.id.container, newsFragment);
                 }
                 else {
                     findViewById(R.id.recycleView).setVisibility(View.GONE);
@@ -44,5 +64,6 @@ public class GeneralActivity extends AppCompatActivity {
                 return true;
             }
         };
+            transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/john/pc_2/fragmant/NewsFragment.java
@@ -0,0 +1,20 @@
+package com.example.john.pc_2.fragmant;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.john.pc_2.R;
+
+public class NewsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kiosk, null);
+    }
+}
