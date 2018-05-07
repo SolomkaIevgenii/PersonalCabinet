c7f12a263fc8d8d6f06f5b71522ccac35f5046e0
Solomka Ievgenii <solomka1989@gmail.com>
2018-05-07 17:37:23 +0300

    07.05 qqq

--- app/src/main/java/com/example/john/pc_2/GeneralActivity.java
+++ app/src/main/java/com/example/john/pc_2/GeneralActivity.java
@@ -2,11 +2,13 @@ package com.example.john.pc_2;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 //import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -16,17 +18,13 @@ import com.example.john.pc_2.fragmant.NewsFragment;
 
 //import java.util.List;
 
-public class GeneralActivity extends FragmentActivity {
+public class GeneralActivity extends AppCompatActivity {
 
     /*Логин и пароль с LoginActivity*/
     public static final String EXTRA_KEY_LOGIN = "Логин";
     public static final String EXTRA_KEY_PASSWORD = "Пароль";
 
-    private KioskFragment kioskFragment;
-    private NewsFragment newsFragment;
 
-    private FragmentManager manager;
-    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +33,6 @@ public class GeneralActivity extends FragmentActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(getBottomNavigationListener());
-
-        kioskFragment = new KioskFragment();
-        newsFragment = new NewsFragment();
     }
 
     @NonNull
@@ -47,23 +41,51 @@ public class GeneralActivity extends FragmentActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                transaction = manager.beginTransaction();
+                Fragment fragment = null;
 
-                if (item.getItemId() == R.id.app_KioskMain){
-                    findViewById(R.id.recycleView).setVisibility(View.VISIBLE);
-                }
-                else if (item.getItemId()==R.id.app_NewsMain){
+                switch (item.getItemId()){
 
-                    //transaction.add(R.id.container, newsFragment);
+                    case R.id.app_KioskMain:
+                        fragment = new KioskFragment();
+                        break;
+                    case R.id.app_NewsMain:
+                        fragment = new NewsFragment();
+                        break;
+                    case R.id.app_NotificationsMain:
+                        Toast.makeText(GeneralActivity.this, "Вот так вот", Toast.LENGTH_SHORT).show();
+                        break;
+                    case  R.id.app_BlockedTransactions:
+                        Toast.makeText(GeneralActivity.this, "Вот так вот 2", Toast.LENGTH_SHORT).show();
+                        break;
                 }
-                else {
-                    findViewById(R.id.recycleView).setVisibility(View.GONE);
-                    Toast.makeText(GeneralActivity.this, "Вот так вот", Toast.LENGTH_SHORT).show();
-                }
-
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.fragmentGeneral, fragment);
+                ft.commit();
                 return true;
             }
         };
-            transaction.commit();
     }
+
 }
+
+
+/*
+public void Change (View view){
+        Fragment fragment = null;
+
+        if (view.getId() == R.id.app_KioskMain){
+            fragment = new KioskFragment();
+        }
+        else if (view.getId() == R.id.app_NewsMain){
+            fragment = new NewsFragment();
+        }
+        else {
+            Toast.makeText(this, "qwwertg", Toast.LENGTH_SHORT).show();
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentGeneral, fragment);
+        ft.commit();
+    }
+*/
